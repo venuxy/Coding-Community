d4f835c1fb6339cdf93599e85d3d0e048be54990
venux <544536852@qq.com>
2024-10-08 00:37:22 +0800

    完成了分页基础实现类的封装

---
+++ subject/common/src/main/java/com/venux/subject/common/entity/PageInfo.java
@@ -0,0 +1,28 @@
+package com.venux.subject.common.entity;
+
+/**
+ * 分页请求实体
+ *
+ * @author: venux
+ * @date: 2024/10/8
+ */
+public class PageInfo {
+    private Integer pageNo = 1;
+    private  Integer pageSize = 20;
+
+    public Integer getPageNo() {
+        if(pageNo == null || pageNo < 1) {
+            return 1;
+        }
+        return pageNo;
+    }
+
+    public Integer getPageSize() {
+        if(pageSize == null || pageSize < 1 || pageSize > Integer.MIN_VALUE) {
+            return 20;
+        }
+        return pageSize;
+    }
+
+
+}
