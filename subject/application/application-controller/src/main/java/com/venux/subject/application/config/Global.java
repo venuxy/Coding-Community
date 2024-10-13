b8a9b8cf6586144c8e3b7fd712e9667b7bb914c7
venux <544536852@qq.com>
2024-10-13 23:45:57 +0800

    查询题目列表时，对空值进行全局处理

--- subject/application/application-controller/src/main/java/com/venux/subject/application/config/Global.java
+++ subject/application/application-controller/src/main/java/com/venux/subject/application/config/Global.java
@@ -1,6 +1,7 @@
 package com.venux.subject.application.config;
 
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +26,7 @@ public class Global extends WebMvcConfigurationSupport {
     private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(){
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return new MappingJackson2HttpMessageConverter(objectMapper);
     }
 
