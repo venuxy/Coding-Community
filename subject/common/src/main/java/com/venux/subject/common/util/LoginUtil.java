cc00e843316dffd881961c2a0035e2fd5ea3c2dc
venux <544536852@qq.com>
2024-10-14 14:37:35 +0800

    使用sql拦截器自动翻译并打印sql日志

---
+++ subject/common/src/main/java/com/venux/subject/common/util/LoginUtil.java
@@ -0,0 +1,19 @@
+package com.venux.subject.common.util;
+
+
+import com.venux.subject.common.context.LoginContextHolder;
+
+/**
+ * 用户登录util
+ *
+ * @author: ChickenWing
+ * @date: 2023/11/26
+ */
+public class LoginUtil {
+
+    public static String getLoginId() {
+        return LoginContextHolder.getLoginId();
+    }
+
+
+}
