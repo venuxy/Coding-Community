4df197e3881a758e97c1f1832cc6ab08535eae6c
venuxy <544536852@qq.com>
2024-10-21 10:24:44 +0800

    集成nacos动态配置，oss配合nacos实现动态配置切换，nacos实现文件bean动态切换

--- oss/src/main/java/com/venux/oss/config/StorageConfig.java
+++ oss/src/main/java/com/venux/oss/config/StorageConfig.java
@@ -4,6 +4,7 @@ import com.venux.oss.adapter.AliStorageAdapter;
 import com.venux.oss.adapter.MinioStorageAdapter;
 import com.venux.oss.adapter.StorageAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +12,14 @@ import org.springframework.context.annotation.Configuration;
  * 文件存储config
  */
 @Configuration
+@RefreshScope
 public class StorageConfig {
 
     @Value("${storage.service.type}")
     private String storageType;
 
     @Bean
-    
+    @RefreshScope
     public StorageAdapter storageService() {
         if ("minio".equals(storageType)) {
             return new MinioStorageAdapter();
